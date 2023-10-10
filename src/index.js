68e56a2456d1e58ed7d4eeb36243abdc427e6a57
ValeriaStav <valeriiastavriadi@gmail.com>
2023-10-10 23:26:42 +0300

    feat: complete task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let num = Math.abs(n);
+
+    let result = num.toString().split("").reverse().join("");
+
+    return result;
+};
